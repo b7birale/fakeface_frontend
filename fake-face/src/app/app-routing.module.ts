f72f351f51d59d1e9a20059575fbe1141f2d7744
Biró Alexandra Dóra <h146951@stud.u-szeged.hu>
2024-04-25 16:04:38 +0200

    Szépítés, tördelés

--- fake-face/src/app/app-routing.module.ts
+++ fake-face/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'not-found', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }];
+const routes: Routes = [
+  { 
+    path: 'not-found', 
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
