4ffac6a791678604d567218b27ea2fc87631bd31
Biró Alexandra Dóra <h146951@stud.u-szeged.hu>
2024-04-27 15:59:26 +0200

    alaphelyzet és not-found betöltődés

--- fake-face/src/app/app-routing.module.ts
+++ fake-face/src/app/app-routing.module.ts
@@ -17,6 +17,15 @@ const routes: Routes = [
   { 
     path: 'not-found', 
     loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) 
+  },
+  { 
+    path: '', 
+    redirectTo: '/feed',
+    pathMatch: 'full'
+  },
+  { 
+    path: '**', 
+    redirectTo: '/not-found'
   }
 ];
 
