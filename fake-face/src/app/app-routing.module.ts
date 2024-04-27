854963668498ea77f022b6ca26dc55b9a95971df
Biró Alexandra Dóra <h146951@stud.u-szeged.hu>
2024-04-27 15:38:32 +0200

    feed modul működik, menu.component.html-ben a menü átírva nav-ra -> ezt még szépíteni kell, de mműködőképes

--- fake-face/src/app/app-routing.module.ts
+++ fake-face/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { 
+    path: 'feed', 
+    loadChildren: () => import('./pages/feed/feed.module').then(m => m.FeedModule) 
+  },
+  { 
     path: 'not-found', 
     loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) 
   }
--- fake-face/src/app/app.module.ts
+++ fake-face/src/app/app.module.ts
@@ -3,26 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeedComponent } from './pages/feed/feed.component';
+//import { FeedComponent } from './pages/feed/feed.component';
 import { FriendsComponent } from './pages/friends/friends.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { MenuComponent } from './shared/menu/menu.component';
-import { FormsModule } from '@angular/forms';
-import { DateFormatPipe } from './shared/pipes/date-format.pipe';
+//import { FormsModule } from '@angular/forms';
+//import { DateFormatPipe } from './shared/pipes/date-format.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeedComponent,
+    //FeedComponent,
     FriendsComponent,
     ProfileComponent,
-    MenuComponent,
-    DateFormatPipe
+    MenuComponent
+    //DateFormatPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
+    //FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
